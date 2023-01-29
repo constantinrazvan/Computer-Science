3804c8e66c8668af2581a17650985d86a20c0d1b
constantinrazvan <razvanpana20@gmail.com>
2023-01-29 23:19:46 +0200

    main

---
+++ BlockLetter - Project 1/initials.py
@@ -0,0 +1,11 @@
+# Razvan Constantin
+# Fun Fact: I made a highschool with different profile than computer science
+
+print("RRRR      CCC")
+print("R   R    C   C")
+print("R   R    C")
+print("RRRR     C")
+print("R  R     C")
+print("R   R    C    C")
+print("R    R    CCC")
+
